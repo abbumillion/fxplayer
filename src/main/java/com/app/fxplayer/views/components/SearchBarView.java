940343c36e1675c6d0ac12a0f38a0a90c4cdebf2
Abbumillion <sharbemillion599@gmail.com>
2023-06-02 20:30:44 +0300

    fxplayer project day 3

--- src/main/java/com/app/fxplayer/views/components/SearchBarView.java
+++ src/main/java/com/app/fxplayer/views/components/SearchBarView.java
@@ -3,26 +3,34 @@ package com.app.fxplayer.views.components;
 import com.app.fxplayer.views.View;
 import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class SearchBarView extends View {
     private TextField searchTextField;
+    private HBox hBox;
     @Override
     public void init() {
         searchTextField = new TextField("search something here");
+        hBox = new HBox();
     }
 
     @Override
     public void build() {
-        getChildren().addAll(searchTextField);
+        hBox.getChildren().add(searchTextField);
+        getChildren().addAll(hBox);
     }
 
     @Override
     public void align() {
         setAlignment(Pos.CENTER);
+        hBox.setAlignment(Pos.CENTER);
     }
 
     @Override
     public void bind() {
-//        searchTextField.prefWidthProperty().bind(widthProperty().multiply(.6));
+        searchTextField.prefWidthProperty().bind(hBox.widthProperty().multiply(.365));
+        searchTextField.prefHeightProperty().bind(hBox.heightProperty().multiply(.5));
+        hBox.prefWidthProperty().bind(widthProperty().multiply(.75));
+
     }
 }
